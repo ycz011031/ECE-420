8647f8dd64f745e8fb9a218961085f6bf07245f2
unknown <yz69@illinois.edu>
2024-03-02 21:59:34 -0600

    lab6 python conv2

--- Lab 06/lab6_python/lab6_conv2.py
+++ Lab 06/lab6_python/lab6_conv2.py
@@ -4,100 +4,133 @@
 import numpy
 from scipy import misc
 import matplotlib.pyplot as plt
+from imageio import imread
+import numpy as np
+
 
 # Function Definition Here
+
+def conv2_1pix(pic_,kernel,i,j,M,N):
+    value_out = 0
+    [l,w] = kernel.shape
+    off_l = int((l-1)/2)
+    off_w = int((w-1)/2)
+    for x in range(l):
+        for y in range(w):
+            if (i-off_l+x<0) or (j-off_w+y<0) or (i-off_l+x>=M) or (j-off_w+y >= N):
+                pic_data=0
+            else:
+                #print (i-off_l+x,j-off_w+y)
+                pic_data = pic_[i-off_l+x][j-off_w+y]
+            pic_data = float(pic_data)    
+            value_out += pic_data * kernel[x][y]
+            #print (np.uint8(value_out))
+    return int(value_out)
 
 # Implement This funtion
 def conv2(pic,kernel):
     # Create a new pic with same size but float type
-    pic_conv = numpy.zeros(numpy.shape(pic))
+    pic_conv = numpy.zeros(numpy.shape(pic), dtype=int)
+    [M,N,T] = pic.shape
+    print (M,N,T)
+    print(kernel)
     # Perform 2-D Convolution with the given kernel
-
-    return pic_conv;
+    for layer in range(3):
+        pic_ = pic[:,:,layer]
+        for i in range (M):
+            for j in range (N):
+                pic_conv[i][j][layer] = int(conv2_1pix(pic_,kernel,i,j,M,N))
+                
+        print ("layer ",layer, "completed")
+        print (pic_)
+        print (pic_conv[:,:,layer])
+    return pic_conv
 
 # Gaussian Kernel Following the Descriptiong: http://www.mathworks.com/help/images/ref/fspecial.html
 def gengaussian(size=5,sigma=3.0):
     if size%2==0 or size<2:
-        print('Size Not Valid');
-        return None;
-    kernel = numpy.zeros((size,size));
+        print('Size Not Valid')
+        return None
+    kernel = numpy.zeros((size,size))
     for x in range(size):
         for y in range(size):
-            kernel[x][y] = numpy.exp(-((x-(size-1)/2)**2+(y-(size-1)/2)**2)/(2*sigma**2));
-    kernel = kernel / numpy.sum(kernel);
+            kernel[x][y] = numpy.exp(-((x-(size-1)/2)**2+(y-(size-1)/2)**2)/(2*sigma**2))
+    kernel = kernel / numpy.sum(kernel)
     return kernel
 
 # Edge Detection Kernel Source:https://alwaysbusycorner.com/2011/12/02/realbasic-canvas-tutorial-lesson-11-edge-detection-kernel-builder/  
 def genxkernel(flag=1):
     if flag == 1:
-        kernel = numpy.array([[-1,0,1]]*3);
+        kernel = numpy.array([[-1,0,1]]*3)
     else:
-        kernel = numpy.array([[-1,0,1],[-2,0,-2],[-1,0,-1]]);
+        kernel = numpy.array([[-1,0,1],[-2,0,-2],[-1,0,-1]])
     return kernel
 
 def genykernel(flag=1):
     if flag == 1:
-        kernel = numpy.array([[-1,-1,-1],[0,0,0],[1,1,1]]);
+        kernel = numpy.array([[-1,-1,-1],[0,0,0],[1,1,1]])
     else:
-        kernel = numpy.array([[-1,-2,-1],[0,0,0],[1,2,1]]);
+        kernel = numpy.array([[-1,-2,-1],[0,0,0],[1,2,1]])
     return kernel
 
 # Merge Detected X-Edge and Y-Edge
 def merge(picx,picy):
-    picshape = numpy.shape(picx);
+    picshape = numpy.shape(picx)
     if picshape != numpy.shape(picy):
-        print('Pic Size Not Matched!');
-        return picx;
-    picmerge = numpy.zeros(picshape);
+        print('Pic Size Not Matched!')
+        return picx
+    picmerge = numpy.zeros(picshape)
     for row in range(picshape[0]):
         for col in range(picshape[1]):
             for channel in range(picshape[2]):
-                picmerge[row][col][channel] = numpy.sqrt((picx[row][col][channel]**2+picy[row][col][channel]**2)/2);
-    picmerge = picmerge.astype(picx.dtype,copy=False);
-    return picmerge;
+                picmerge[row][col][channel] = numpy.sqrt((picx[row][col][channel]**2+picy[row][col][channel]**2)/2)
+    picmerge = picmerge.astype(picx.dtype,copy=False)
+    return picmerge
     
 # Main Function Starts Here!!!
 # Gaussian Blur Kernel
 # Read Image and Display
-kitten_origin = misc.imread('kitten.png');
-fig_kitten_origin = plt.figure(1);
-fig_kitten_origin.suptitle('Original Kitten.png', fontsize=14, fontweight='bold');
-plt.imshow(kitten_origin,vmin = 0, vmax = 255);
-plt.show(block=False);
+kitten_origin = imread('kitten.png')
+fig_kitten_origin = plt.figure(1)
+fig_kitten_origin.suptitle('Original Kitten.png', fontsize=14, fontweight='bold')
+plt.imshow(kitten_origin,vmin = 0, vmax = 255)
+plt.show(block=False)
 # Generate Kernel
-kernel_blur = gengaussian(3);
+kernel_blur = gengaussian(3)
 # Apply Convolution
 kitten_blur = conv2(kitten_origin,kernel_blur)
 # Display Results
-fig_kitten_blur = plt.figure(2);
-fig_kitten_blur.suptitle('Blurred Kitten.png', fontsize=14, fontweight='bold');
-plt.imshow(kitten_blur,vmin = 0, vmax = 255);
-plt.show(block=False);
+fig_kitten_blur = plt.figure(2)
+fig_kitten_blur.suptitle('Blurred Kitten.png', fontsize=14, fontweight='bold')
+plt.imshow(kitten_blur,vmin = 0, vmax = 255)
+plt.show(block=False)
 
+
+#breakpoint()
 # Edge Detection Kernel
 # Read Image and Display
-logo_origin = misc.imread('logo.png');
-fig_logo_origin = plt.figure(3);
-fig_logo_origin.suptitle('Original Logo.png', fontsize=14, fontweight='bold');
-plt.imshow(logo_origin,vmin = 0, vmax = 255);
-plt.show(block=False);
+logo_origin = imread('logo.png')
+fig_logo_origin = plt.figure(3)
+fig_logo_origin.suptitle('Original Logo.png', fontsize=14, fontweight='bold')
+plt.imshow(logo_origin,vmin = 0, vmax = 255)
+plt.show(block=False)
 # X-Edge Detection
-kernel_xedge = genxkernel();
+kernel_xedge = genxkernel()
 logo_xedge = conv2(logo_origin,kernel_xedge)
-fig_logo_xedge = plt.figure(4);
-fig_logo_xedge.suptitle('X-Edge Detected Logo.png', fontsize=14, fontweight='bold');
-plt.imshow(logo_xedge,vmin = 0, vmax = 255);
-plt.show(block=False);
+fig_logo_xedge = plt.figure(4)
+fig_logo_xedge.suptitle('X-Edge Detected Logo.png', fontsize=14, fontweight='bold')
+plt.imshow(logo_xedge,vmin = 0, vmax = 255)
+plt.show(block=False)
 # Y-Edge Detection
-kernel_yedge = genykernel();
+kernel_yedge = genykernel()
 logo_yedge = conv2(logo_origin,kernel_yedge)
-fig_logo_yedge = plt.figure(5);
+fig_logo_yedge = plt.figure(5)
 fig_logo_yedge.suptitle('Y-Edge Detected Logo.png', fontsize=14, fontweight='bold');
-plt.imshow(logo_yedge,vmin = 0, vmax = 255);
-plt.show(block=False);
+plt.imshow(logo_yedge,vmin = 0, vmax = 255)
+plt.show(block=False)
 # Merge Edges
-logo_fulledge = merge(logo_xedge,logo_yedge);
-fig_logo_fulledge = plt.figure(6);
+logo_fulledge = merge(logo_xedge,logo_yedge)
+fig_logo_fulledge = plt.figure(6)
 fig_logo_fulledge.suptitle('Full-Edge Detected Logo.png', fontsize=14, fontweight='bold');
-plt.imshow(logo_fulledge,vmin = 0, vmax = 255);
-plt.show();
+plt.imshow(logo_fulledge,vmin = 0, vmax = 255)
+plt.show()
